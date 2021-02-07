63946d5807c07efc39093dc29557278a01eaafdd
ayalajt <ayalajt@vcu.edu>
2021-02-06 22:59:44 -0500

    Created skeleton of responses and strengthened quit function

--- eliza.py
+++ eliza.py
@@ -5,35 +5,48 @@ import time
 # Can possibly call another function that picks a correct question from the given input
 
 infiniteLoop = True
+introChat = False
+toQuit = False
 
 def chooseResponse(userInput):
-    if (re.search("feeling", userInput)):
-        print("[SYBIL]: Why are you feeling sad")
+    if (re.search("feeling (.*)", userInput)):
+        match = re.search("feeling (.*)", userInput)
+        print("[SYBIL]: Why are you feeling {}?".format(match.group(1)))
+    if (re.search("feel (.*)", userInput)):
+        match = re.search("feel (.*)", userInput)
+        print("[SYBIL]: Why do you feel {}?".format(match.group(1)))
+    #if (re.search("feel (.*)", userInput)):
+     #   match = re.search("feel (.*)", userInput)
+      #  print("[SYBIL]: Why do you feel {}?".format(match.group(1)))
 
 def checkQuit(userInput):
      if re.match("quit", userInput):
             print("[SYBIL]: Goodbye!")
-            return True
+            quit()
+
+def introText(userInput):
+    # need to figure out how to quit from here
+    return True
 
 def main():
     global infiniteLoop
     introChat = False
     while infiniteLoop:
-        if introChat == False:
-            print("[SYBIL]: Hello! My name is Sybil. What's your name?") 
+        while introChat == False:
+            print("[SYBIL]: Hello! I am Sybil, a psychotherapist. What's your name?") 
             userInput = input("[USER]: ")
-            if checkQuit(userInput): break
+            checkQuit(userInput)
             if re.match("^[A-Za-z]*$", userInput):
                 userName = userInput
-                print('[SYBIL]: Nice to meet you {}!'.format(userName))
-                time.sleep(1)
-                print('[SYBIL]: What brings you here today {}?'.format(userName))
+                print('[SYBIL]: Nice to meet you {}! What brings you here today?'.format(userName))
                 introChat = True
             else:
                 print("[SYBIL]: Please input a valid name.")
+
         # After intro has played
+
         userInput = input("[{}]: ".format(userName.upper()))
-        if checkQuit(userInput): break
+        checkQuit(userInput)
         chooseResponse(userInput)
 
 if __name__ == '__main__':
