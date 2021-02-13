ece4ba72f73d14fa512cf18364fcaa268b2eadec
ayalajt <ayalajt@vcu.edu>
2021-02-12 23:51:26 -0500

    Added more responses

--- eliza.py
+++ eliza.py
@@ -1,5 +1,6 @@
 import re
 import time
+import random
 
 # Can possibly making enums such as FEELINGS to keep track of all possible emotions
 # Can possibly call another function that picks a correct question from the given input
@@ -7,18 +8,49 @@ import time
 infiniteLoop = True
 introChat = False
 toQuit = False
+userName = "null"
+
+def changeWords(userInput):
+    if re.match("^I ", userInput):
+        userInput = re.sub("^I ", "you ", userInput)
+    if re.match(r"\sI\s", userInput):
+        userInput = re.sub(r"\sI\s", " you ", userInput)
+    return userInput
 
 def chooseResponse(userInput):
-    if (re.search("feeling (.*)", userInput)):
-        match = re.search("feeling (.*)", userInput)
+    ## could also change a word's tense before looking for a valid reponse, i.e. cried -> cry
+    if (re.search("feeling (.+)", userInput)):
+        match = re.search("feeling (.+)", userInput)
         print("[SYBIL]: Why are you feeling {}?".format(match.group(1)))
-    if (re.search("feel (.*)", userInput)):
-        match = re.search("feel (.*)", userInput)
+    elif (re.search("feel (.+)", userInput)):
+        match = re.search("feel (.+)", userInput)
         print("[SYBIL]: Why do you feel {}?".format(match.group(1)))
-    #if (re.search("feel (.*)", userInput)):
-     #   match = re.search("feel (.*)", userInput)
-      #  print("[SYBIL]: Why do you feel {}?".format(match.group(1)))
-
+    elif (re.search("want to (.+)", userInput)):
+        match = re.search("want to (.+)", userInput)
+        print("[SYBIL]: {}, Why do you want to {}?".format(userName, match.group(1)))
+    elif (re.search("(B|b)ecause my (.+)", userInput)):
+        match = re.search("(B|b)ecause my (.+)", userInput)
+        print("[SYBIL]: {}, Why did your {}?".format(userName, match.group(2)))
+    elif (re.search("I need (.+)", userInput)):
+        #userInput = changeWords(userInput)
+        match = re.search("I need (.+)", userInput)
+        print("[SYBIL]: Why do you need {}, {}?".format(match.group(1), userName))
+    elif (re.search("I think (.+)", userInput)):
+        match = re.search("I think (.+)", userInput)
+        print("[SYBIL]: Why do you think {}, {}?".format(match.group(1), userName))
+    else:
+        randomChoiceNum = random.randint(1,5)
+        if randomChoiceNum == 1:
+            print("[SYBIL]: Please, tell me more.")
+        if randomChoiceNum == 2: 
+            print("[SYBIL]: Could you explain some more?")
+        if randomChoiceNum == 3:
+            print("[SYBIL]: Please, could you elaborate some more?")
+        if randomChoiceNum == 4:
+            print("[SYBIL]: Is there more to that?")
+        if randomChoiceNum == 5:
+            print("[SYBIL]: Why do you say that?")
+       
 def checkQuit(userInput):
      if re.match("quit", userInput):
             print("[SYBIL]: Goodbye!")
@@ -30,21 +62,28 @@ def introText(userInput):
 
 def main():
     global infiniteLoop
+    global userName
     introChat = False
     while infiniteLoop:
         while introChat == False:
             print("[SYBIL]: Hello! I am Sybil, a psychotherapist. What's your name?") 
             userInput = input("[USER]: ")
             checkQuit(userInput)
-            if re.match("^[A-Za-z]*$", userInput):
-                userName = userInput
+            if re.match(r"My (N|n)ame is ([A-Za-z]+)(\.*)", userInput):
+                userName = re.search(r"My (N|n)ame is ([A-Za-z]+)(\.*)", userInput)
+                userName = userName.group(2)
                 print('[SYBIL]: Nice to meet you {}! What brings you here today?'.format(userName))
                 introChat = True
             else:
-                print("[SYBIL]: Please input a valid name.")
+                if re.match("^[A-Za-z]*$", userInput):
+                    userName = userInput
+                    print('[SYBIL]: Nice to meet you {}! What brings you here today?'.format(userName))
+                    introChat = True
+                else:
+                    print("[SYBIL]: Please input a valid name.")
 
         # After intro has played
-
+    
         userInput = input("[{}]: ".format(userName.upper()))
         checkQuit(userInput)
         chooseResponse(userInput)
